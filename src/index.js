81896bbebfe37a93dc7b84146489260b6865e06b
Anastasiia Zahuliaieva <nastyazagyl2@gmail.com>
2020-10-19 16:01:44 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = reverse;
+
+function reverse (n) {
+    let a;
+    let result = 0;
+    n < 0 ? a = Math.abs(n) : a = n;
+    while (a) {
+        result = result * 10 + a % 10;
+        a = Math.floor(a / 10);
+    }
+    return result;
 }
+
+console.log(reverse(520));
